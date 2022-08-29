2d09001fb9c9c882d56365d3eaf3913d069c12e2
Armaan Sidhu <74760354+roastflix@users.noreply.github.com>
2022-08-29 19:17:39 +0530

    Add files via upload

---
+++ Module 1/RepetetiveTasksWithLoops.js
@@ -0,0 +1,24 @@
+var n = 5;
+for(var i=0; i<n; i++) {
+    console.log(i+1);
+}
+console.log("Counting Completed");
+
+var i = 0;
+while(i < 5) {
+    console.log(5-i);
+    i++;
+}
+console.log("Counting Completed");
+
+var i = 5;
+while(i--) {
+    console.log(i+1);
+}
+console.log("Countdown Finished");
+
+var year = 2018;
+while(year < 2023) {
+    console.log(year);
+    year++;
+}
