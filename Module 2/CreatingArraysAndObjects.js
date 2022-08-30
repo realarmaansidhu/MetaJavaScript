0acfb98cd118b2fecec8aeb5949630d1b60af6a9
Armaan Sidhu <74760354+roastflix@users.noreply.github.com>
2022-08-30 11:49:06 +0530

    Add files via upload

---
+++ Module 2/CreatingArraysAndObjects.js
@@ -0,0 +1,14 @@
+var clothes = [];
+clothes.push("Denims");
+clothes.push("Jeans");
+clothes.push("Shorts");
+clothes.push("Jackets");
+clothes.push("Sweatshirts");
+clothes.pop("Sweatshirts");
+clothes.push("Cargos")
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "Blue";
+favCar.convertible = "true";
+console.log(favCar);
